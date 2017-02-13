b426f51deadd4e71d4022c98b5bd59149cdbc6ae
wukai <wukai@wukaideMac-mini.local>
2017-02-13 11:14:15 +0800

    加入模版

--- app.js
+++ app.js
@@ -5,7 +5,15 @@ var app = express();
  */
 //app.use(express.static(__dirname+'/public'));
 app.use('/static', express.static(__dirname + '/public'));
-
+/**
+ * 使项目支持html模板，使用和ejs模板一样
+ */
+var ejs = require('ejs');
+app.engine('.html',ejs.__express);
+//设置模版引擎
+app.set('view engine','html');
+//设置模版文件所在目录
+app.set('views','./views');
 
 app.get('/', function (req, res) {
   res.send('Hello World!');
