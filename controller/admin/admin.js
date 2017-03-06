a06735eae8ee3cdb046044ce1755d7c0cc321ca9
wukai <wukai@wukaideMac-mini.local>
2017-03-06 18:32:04 +0800

    login

--- controller/admin/admin.js
+++ controller/admin/admin.js
@@ -4,6 +4,8 @@
 
 var common = require('../../util/common');
 var config = require('../../config');
+var validator = require('validator');
+var AdminUser = require('../../model/AdminUser');
 module.exports = {
     /**
      * 登录页面
@@ -20,7 +22,13 @@ module.exports = {
         var newPsd = common.encrypt(password,config.encrypt_key);
         var captcha = req.body.captcha;
         if(common.compareString(req.session.captcha,captcha)){
-
+            if(validator.isUserName(username) && validator.isPsd(password)){
+                AdminUser.findOne({username:username,password:newPsd}).populate('group').exec(function(err,user){
+                    if(err){
+                        return res.end(err);
+                    }
+                })
+            }
         }else{
             //验证码错误
             return res.json({'status':'captcha error'});
